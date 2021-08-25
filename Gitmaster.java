50e7f478edd90ea330207cf133aacd6d22414452
vijay <bandivijay.vk@gmail.com>
2021-08-25 13:15:36 +0530

    master

---
+++ Gitmaster.java
@@ -0,0 +1,13 @@
+package Gitmaster;
+
+public class Gitmaster {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome");
+
+System.out.println("welcome");
+System.out.println("welcome");
+	}
+
+
+}
